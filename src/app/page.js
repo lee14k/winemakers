15590b1ddcb2e1efc6106469720c9c167ec2ade2
lee14k <kmkovach@mtu.edu>
2023-08-04 00:36:23 -0400

    fixed navbar
    it was issues with spacing - added padding to nav and that fixed it

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Footer from "../../Components/Footer"
 import Navbar from "../../Components/Navbar"
 export default function Home() {
   return (
-   <div className="pagewrap">
+   <div>
     <Navbar/>
 <Headbanner/>  
 <Midsection/>  
