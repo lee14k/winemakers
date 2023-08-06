e4735b50a9de86b3b0e357fa2d6f9a991a3a9e3a
lee14k <kmkovach@mtu.edu>
2023-08-06 17:56:37 -0400

    adding mid section backl

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Headbanner from "../../Components/Headbanner"
 import Homeevents from "../../Components/Homeevents"
 import Footer from "../../Components/Footer"
 import Navbar from "../../Components/Navbar"
+import Middsex from "../../Components/Midsex"
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
     <Navbar/>
 <Headbanner/>  
 <Homeevents/>
+<Middsex/>
 <Footer/>
    </>
   )
