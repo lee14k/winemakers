17af395e992a1f9554e34381e9a8dd4223586342
lee14k <kmkovach@mtu.edu>
2023-08-06 17:41:53 -0400

    trying to fix

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,7 @@ import Headbanner from "../../Components/Headbanner"
 import Homeevents from "../../Components/Homeevents"
 import Footer from "../../Components/Footer"
 import Navbar from "../../Components/Navbar"
-<<<<<<< HEAD
-import Newmid from "../../Components/Newmid"
-import Midsex from "../../Components/Midsex"
-=======
 import Midsex from "../../Components/Midsex" 
->>>>>>> main
 export default function Home() {
   return (
    <>
