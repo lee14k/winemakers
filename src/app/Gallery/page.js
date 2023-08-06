12102c87a7fd98eaf809a0b2731bc0cda50a784b
lee14k <kmkovach@mtu.edu>
2023-08-06 17:53:42 -0400

    continued vercel issues

--- src/app/Gallery/page.js
+++ src/app/Gallery/page.js
@@ -1,12 +1,11 @@
+'use client'
 import Imagegallery from "../../../Components/Imagegallery";
 import Navbar from "../../../Components/Navbar";
-import Midsex from "../../../Components/Midsex";
 export default function Gallery () {
   return (
     <div>
       <Navbar/>
       <Imagegallery />
-      <Midsex/>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,12 @@ import Headbanner from "../../Components/Headbanner"
 import Homeevents from "../../Components/Homeevents"
 import Footer from "../../Components/Footer"
 import Navbar from "../../Components/Navbar"
-import Midsex from "../../Components/Midsex" 
+
 export default function Home() {
   return (
    <>
     <Navbar/>
 <Headbanner/>  
-<Midsex/>
 <Homeevents/>
 <Footer/>
    </>
