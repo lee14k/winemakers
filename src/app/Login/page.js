1e823b6e19eeaf0db406d0ec8fe1ab241897dce5
lee14k <kmkovach@mtu.edu>
2023-08-23 10:58:25 -0400

    trying to make this work

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -5,5 +5,6 @@ import Footer from '../../../Components/Footer';
 import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Login() {
+  const {user, error, isLoading} = useUser();
   return <a href="/api/auth/login">Login</a>;
 }
