53e4fe13791ea1e8b5200f3ff355f67eb5130cd9
lee14k <kmkovach@mtu.edu>
2023-08-23 19:18:35 -0400

    auth0

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -2,9 +2,7 @@
 import { useState, useEffect } from 'react';
 import Navbar from '../../../Components/Navbar';
 import Footer from '../../../Components/Footer';
-import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Login() {
-  const {user, error, isLoading} = useUser();
-  return <a href="/api/auth/login">Login</a>;
+  return <a href="/pages/api/auth/login">Login</a>;
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Homeevents from "../../Components/Homeevents"
 import Footer from "../../Components/Footer"
 import Navbar from "../../Components/Navbar"
 import Middsex from "../../Components/Midsex"
-import { UserProvider } from '@auth0/nextjs-auth0';
 export default function Home() {
   return (
    <>
