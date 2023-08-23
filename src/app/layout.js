733ec322f904e7ee082890b2a25ed29726d56fe2
lee14k <kmkovach@mtu.edu>
2023-08-23 19:30:39 -0400

    trying to restructure for auth0

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-          <UserProvider>
+          <UserProvider loginUrl="/winemakers/api/auth/login">
 
       <body className={inter.className}>{children}</body>
       </UserProvider>
